9f0760c12cc6ec58315f7f12e7d1cc22a83b2309
Matheus Henrique <matheus.181137@gmail.com>
2018-12-01 04:58:23 -0300

    VOU ESCOVAR O DENTE

--- Marcador de calor/js/main.js
+++ Marcador de calor/js/main.js
@@ -24,60 +24,16 @@ document.addEventListener('DOMContentLoaded', function() {
       var instances = M.Tooltip.init(elems, {margin: 10});
 });
 
-// mapinha da api topzera lá
-var heatmapData = [
-  new google.maps.LatLng(37.782, -122.447),
-  new google.maps.LatLng(37.782, -122.445),
-  new google.maps.LatLng(37.782, -122.443),
-  new google.maps.LatLng(37.782, -122.441),
-  new google.maps.LatLng(37.782, -122.439),
-  new google.maps.LatLng(37.782, -122.437),
-  new google.maps.LatLng(37.782, -122.435),
-  new google.maps.LatLng(37.785, -122.447),
-  new google.maps.LatLng(37.785, -122.445),
-  new google.maps.LatLng(37.785, -122.443),
-  new google.maps.LatLng(37.785, -122.441),
-  new google.maps.LatLng(37.785, -122.439),
-  new google.maps.LatLng(37.785, -122.437),
-  new google.maps.LatLng(37.785, -122.435)
-];
-
-var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
-
-map = new google.maps.Map(document.getElementById('map'), {
-  center: sanFrancisco,
-  zoom: 13,
-  mapTypeId: 'satellite'
-});
-
-var heatmap = new google.maps.visualization.HeatmapLayer({
-  data: heatmapData
-});
-heatmap.setMap(map);
-
 var heatMapData = [
-  {location: new google.maps.LatLng(37.782, -122.447), weight: 0.5},
-  new google.maps.LatLng(37.782, -122.445),
-  {location: new google.maps.LatLng(37.782, -122.443), weight: 2},
-  {location: new google.maps.LatLng(37.782, -122.441), weight: 3},
-  {location: new google.maps.LatLng(37.782, -122.439), weight: 2, height: 20},
-  new google.maps.LatLng(37.782, -122.437),
-  {location: new google.maps.LatLng(37.782, -122.435), weight: 0.5},
-
-  {location: new google.maps.LatLng(37.785, -122.447), weight: 3},
-  {location: new google.maps.LatLng(37.785, -122.445), weight: 2},
-  new google.maps.LatLng(37.785, -122.443),
-  {location: new google.maps.LatLng(37.785, -122.441), weight: 0.5},
-  new google.maps.LatLng(37.785, -122.439),
-  {location: new google.maps.LatLng(37.785, -122.437), weight: 2},
-  {location: new google.maps.LatLng(37.785, -122.435), weight: 3}
+  new google.maps.LatLng(-16.475144, -54.6233646,17),
+  new google.maps.LatLng(-16.476144, -54.6233646,17),
 ];
 
-var sanFrancisco = new google.maps.LatLng(37.774546, -122.433523);
+var sanFrancisco = new google.maps.LatLng(-16.475144, -54.6233646,17);
 
 map = new google.maps.Map(document.getElementById('map'), {
   center: sanFrancisco,
-  zoom: 13,
+  zoom: 15,
   mapTypeId: 'satellite'
 });
 
